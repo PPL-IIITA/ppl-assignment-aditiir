1246ef937c891697c76e97476aec4748c6a5d523
aditiir <lit2015021@iiita.ac.in>
2017-04-03 23:07:05 +0530

    Add files via upload

---
+++ PPL_q6/src/girl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @see girl
+ *This class stores the attributes of a girl
+ */
+public class girl {
+    String name;
+    int attractiveness;
+    int intelligence_level;
+    int maint_cost;
+    char criteria;
+    char type;
+    boolean commit;
+    String name_b;
+}
---
+++ PPL_q6/src/iit069_q6.java
@@ -0,0 +1,19 @@
+
+import java.io.IOException;
+
+/**
+ * @see iit069_q6
+ *This class calls the method of class test6 to 
+ * display the k most happy and compatible couples 
+ * 
+ */
+
+public class iit069_q6
+{
+    public static void main(String args[])throws IOException
+    {
+        test6 t2=new test6();
+        t2.gen_couple();
+    }
+}
+    
