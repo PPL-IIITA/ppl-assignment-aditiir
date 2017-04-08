ee1908665c2708618d7de9853a23e5e7519bd6e7
aditiir <lit2015021@iiita.ac.in>
2017-04-08 13:13:07 -0700

    Add files via upload

---
+++ PPL_q10/src/desperate_girl.java
@@ -0,0 +1,25 @@
+
+import static java.lang.Math.exp;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * @see desperate_girl subclass of class girl
+ */
+
+public class desperate_girl extends girl{
+    /**
+     * An abstract method for calculating the happiness
+     * according to type of boy and girl
+     * @param c an object of class couple
+     * @param g an object of class girl
+     */
+    public void happiness(girl g,couple c)
+    {
+        c.happiness_g=(int) exp(c.gift_cost)-g.maint_cost;
+    }
+    
+}
---
+++ PPL_q10/src/gift.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @see gift Stores the attributes of gifts
+ */
+public class gift {
+    int price,value;
+}
